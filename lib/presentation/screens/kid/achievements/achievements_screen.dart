b53c7da7cbc354b6125dafc40e056bdbb97cb507
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-28 08:03:06 +0530

    Delete lib directory

--- lib/presentation/screens/kid/achievements/achievements_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class AchievementsScreen extends StatelessWidget {
-  const AchievementsScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('AchievementsScreen')));
-}
