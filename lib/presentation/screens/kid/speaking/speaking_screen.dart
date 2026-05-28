fd1c710ede82a706ceee741259b06c0b3996c139
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-28 07:41:01 +0530

    Add files via upload

---
+++ lib/presentation/screens/kid/speaking/speaking_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class SpeakingScreen extends StatelessWidget {
+  const SpeakingScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('SpeakingScreen')));
+}
---
+++ lib/presentation/widgets/kid/streak_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class StreakWidget extends StatelessWidget {
+  final int streak;
+  const StreakWidget({super.key, required this.streak});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.white.withValues(alpha: 0.2),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text('🔥', style: TextStyle(fontSize: 14)),
+          const SizedBox(width: 4),
+          Text(
+            '${streak}d',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
