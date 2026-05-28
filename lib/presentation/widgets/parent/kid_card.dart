a4ac3c25f0724856dc76f6bed0d710269d5561e5
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-28 07:54:55 +0530

    Delete lib directory

--- lib/presentation/widgets/parent/kid_card.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../../../domain/entities/entities.dart';
-class KidCard extends StatelessWidget {
-  final KidEntity kid;
-  const KidCard({super.key, required this.kid});
-  @override
-  Widget build(BuildContext context) => const SizedBox.shrink();
-}
