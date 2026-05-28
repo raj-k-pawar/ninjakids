0e3947aa045bc58845cbcd51223830f6db517e4d
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-28 09:25:49 +0530

    Delete lib directory

--- lib/presentation/screens/parent/analytics.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class ParentAnalyticsScreen extends StatelessWidget {
-  const ParentAnalyticsScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('ParentAnalyticsScreen')));
-}
