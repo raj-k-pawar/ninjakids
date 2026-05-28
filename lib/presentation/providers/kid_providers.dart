32dd352418f0a2b402d1e25231a24e143c07040a
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-28 07:39:07 +0530

    Delete lib directory

--- lib/presentation/providers/kid_providers.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../domain/entities/entities.dart';
-import '../../services/auth/auth_service.dart';
-
-final currentKidProvider = FutureProvider<KidEntity>((ref) async {
-  throw UnimplementedError('Set active kid in auth flow');
-});
