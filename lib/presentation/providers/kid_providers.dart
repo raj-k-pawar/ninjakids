0089399e3a748f820e7c9f600369b6c6dca998f7
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-28 07:47:24 +0530

    Delete lib directory

--- lib/presentation/providers/kid_providers.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../domain/entities/entities.dart';
-
-final currentKidProvider = FutureProvider<KidEntity>((ref) async {
-  throw UnimplementedError('Set active kid in auth flow');
-});
--- lib/presentation/screens/parent/settings/parent_settings_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class ParentSettingsScreen extends StatelessWidget {
-  const ParentSettingsScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('ParentSettingsScreen')));
-}
