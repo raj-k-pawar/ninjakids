b39bff74f42ce8cdd57133c6675fceef31c74c32
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-28 14:17:40 +0530

    Delete lib directory

--- lib/presentation/screens/auth/parent_login_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class ParentLoginScreen extends StatelessWidget {
-  const ParentLoginScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('ParentLoginScreen')));
-}
