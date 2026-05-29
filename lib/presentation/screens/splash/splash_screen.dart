a0576f6c411252d45315a173d86c3da61a239f9d
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-29 16:09:40 +0530

    Delete lib directory

--- lib/presentation/screens/splash/splash_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Splash Screen
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(
-      body: Center(child: Text('🥷', style: TextStyle(fontSize: 64))));
-}
