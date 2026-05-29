2af4e8b06d31edcacf5ca64d5636ba411158ed04
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-29 09:37:36 +0530

    Delete lib directory

--- lib/presentation/screens/kid/games/games_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class GamesScreen extends StatelessWidget {
-  const GamesScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('Games')));
-}
