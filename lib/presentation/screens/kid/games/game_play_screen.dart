268b1210f16c4fb9d196809840b7e2b54a7f734e
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-29 13:27:51 +0530

    Delete lib directory

--- lib/presentation/screens/kid/games/game_play_screen.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-class GamePlayScreen extends StatelessWidget {
-  final String gameId;
-  const GamePlayScreen({super.key, required this.gameId});
-  @override
-  Widget build(BuildContext context) => Scaffold(body: Center(child: Text('Playing: $gameId')));
-}
