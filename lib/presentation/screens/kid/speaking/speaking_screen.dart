111253e22ab84559cd367e559aa8c24a7aff7c70
raj-k-pawar <raj.k.pawar1990@gmail.com>
2026-05-29 16:52:24 +0530

    Delete lib directory

--- lib/presentation/screens/kid/speaking/speaking_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class SpeakingScreen extends StatelessWidget {
-  const SpeakingScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('SpeakingScreen')));
-}
